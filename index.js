0fd677b0342de0f1d1dd46068ba555f7ea200f5d
waranchit4748 <waranchit.ink@dome.tu.ac.th>
2025-02-18 09:52:45 +0700

    ทำไมมัน pool มาไม่หมด

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+const productRoutes = require('./routes/productRoutes');
+app.use('/api', productRoutes);
 // เชื่อมต่อ MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
